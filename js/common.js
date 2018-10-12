edc626d280f5a6db279f800703d9fcfe456f98a2
Kolya Bokov <bokov1981@mail.ru>
2018-10-12 16:32:36 +0300

    insert logo svg code to html & add logo animation

--- js/common.js
+++ js/common.js
@@ -1,4 +1,109 @@
 $(document).ready(function () {
+
+
+
+
+// Start Animate Logo Svg	
+
+let tl = new TimelineMax();
+
+tl
+
+	.from( $('.pol__1'), 1.5, {
+		y: -700
+	})
+
+	.from( $('.pol__2'), 1.5, {
+		y: -700,
+		x: -700
+	},"-=1.5")
+
+	.from( $('.pol__3'), 1.6, {
+		y: -900,
+		x: -900
+	},"-=1.5")
+
+	.from( $('.pol__4'), 1.5, {
+		y: 1200,
+		x: 1200
+	},"-=2.3")
+
+	.from( $('.pol__5'), 1.5, {
+		y: 1200,
+		x: 1200
+	},"-=2")
+
+
+
+	.from( $('.logo__m'), 1.3, {
+		y: -700
+	},"-=1.5")
+
+	.from( $('.logo__o'), 1.3, {
+		y: -700
+	},"-=1.3")
+
+	.from( $('.logo__l'), 1.3, {
+		y: -700
+	},"-=1.3")
+
+
+	.from( $('.logo__second_o'), 1.3, {
+		y: -1000
+	},"-=1.3")
+
+	.from( $('.logo__t'), 1.3, {
+		y: -1500
+	},"-=1.3")
+
+
+
+
+	.from( $('.logo__bottom_m'), 1.1, {
+		y: 700
+	},"-=2")
+
+	.from( $('.logo__bottom_o'), 1.1, {
+		y: 700
+	},"-=1.8")
+	.from( $('.logo__bottom_t'), 1.1, {
+		y: 700
+	},"-=1.6")
+	.from( $('.logo__bottom_second_o'), 1.1, {
+		y: 700
+	},"-=1.5")
+	.from( $('.logo__bottom_r'), 1.1, {
+		y: 700
+	},"-=1.4")
+	.from( $('.logo__bottom_s'), 1.1, {
+		y: 700
+	},"-=1.3")
+
+
+	.from( $('.logo__bottom_g'), 1, {
+		x: 1000
+	},"-=1.25")
+	.from( $('.logo__bottom_second_r'), 1, {
+		x: 1000
+	},"-=1.2")
+	.from( $('.logo__fird_o'), 1, {
+		x: 1000
+	},"-=1.15")
+	.from( $('.logo__bottom_u'), 1, {
+		x: 1000
+	},"-=1.1")
+	.from( $('.logo__bottom_p'), 0.9, {
+		x: 1000
+	},"-=1.05")
+	;
+
+
+
+
+
+// End Animate Logo Svg	
+
+// Start TimeCounter	
 	
 	function getTimeRemaining(endtime) {
 	  var t = Date.parse(endtime) - Date.parse(new Date());
@@ -37,6 +142,11 @@ $(document).ready(function () {
 
 	var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
 	initializeClock('clockdiv', deadline);
+
+
+// End TimeCounter	
+
+
 
 
 });
